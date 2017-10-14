448664d09a85a599ab46636adb7dbe251fa3f03c
Carl Bennett <carlbennettnz@gmail.com>
2017-10-15 12:49:29 +1300

    Enable experimental object rest spread support

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ module.exports = {
 
   parserOptions: {
     ecmaVersion: 2017,
-    sourceType: 'module'
+    sourceType: 'module',
+    ecmaFeatures: {
+        experimentalObjectRestSpread: true
+    }
   },
 
   plugins: [
